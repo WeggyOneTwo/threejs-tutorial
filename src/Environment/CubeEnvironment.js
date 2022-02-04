503e17aac571aa6d29fa50966d1c21aa5d02aac0
WeggyOneTwo <99024474+WeggyOneTwo@users.noreply.github.com>
2022-02-04 15:37:05 +0000

    Finished tutorial

--- src/Environment/CubeEnvironment.js
+++ src/Environment/CubeEnvironment.js
@@ -20,7 +20,7 @@ import AstronautGLB from '../Assets/Models/Astronaut.glb'
 import Chair from '../Assets/Models/Chair.glb'
 import Car from '../Assets/Models/car.glb'
 import { OverlayItem } from '../Utility/Models/OverlayItem';
-import Parrot from '../Assets/Models/Parrot.glb'
+import Mycar from '../Assets/Models/caleb_car.glb'
 const CubeEnvironmentWrapper = styled.div`height: 100vh;`;
 
 /**
@@ -70,9 +70,9 @@ class CubeEnvironment extends Component {
 		};
 		
 		this.overlayItem = new OverlayItem(
-			"Soy Cuba", 
+			"Black Midi, New Road", 
 			"VIDEO", 
-			"https://www.youtube.com/embed/BwEabZrGFfI"
+			"https://www.youtube.com/embed/B8gmGGjWJws"
 		)
       }
 
@@ -109,15 +109,15 @@ class CubeEnvironment extends Component {
 		this.setDimensions();
 		this.setupCamera();
 		// ADD CONTROLS
-		// this.setupControls();
+		this.setupControls();
 		this.setupRenderer();
 		
 		// ADD MODELS
 		// this.setupLoadingManager();
 
 		// MAKE INTERACTIVE
-		// this.setupRayCaster()
-		// this.setupMouse()
+		this.setupRayCaster()
+		this.setupMouse()
 
 		// ADD POST PROCESSING
 		// this.setupPostProcessing();
@@ -134,7 +134,7 @@ class CubeEnvironment extends Component {
 		this.addHelpers();
 		this.addLights();
 		// this.addCube( new THREE.Vector3(0,0,0),this.overlayItem);
-		this.addModel(Parrot, new THREE.Vector3(0,0,0), this.overlayItem);
+		this.addModel(Mycar, new THREE.Vector3(0,0,0), this.overlayItem);
 		// this.setupFog();
 	};
 
@@ -150,7 +150,7 @@ class CubeEnvironment extends Component {
 			0.1, // near plane
 			1000 // far plane
 		);
-		this.camera.position.z =50; // is used here to set some distance from a cube that is located at z = 0
+		this.camera.position.z =0.2; // is used here to set some distance from a cube that is located at z = 0
 	};
 
 	/**
@@ -462,7 +462,7 @@ class CubeEnvironment extends Component {
      */
 	addEventListeners = () => {
 		// MAKE INTERACTIVE
-		// document.addEventListener("dblclick", this.onDocumentDoubleClick, false);
+		document.addEventListener("dblclick", this.onDocumentDoubleClick, false);
 		window.addEventListener('resize', this.handleWindowResize, false);
 	};
 
@@ -473,7 +473,7 @@ class CubeEnvironment extends Component {
 	 */
 	removeEventListeners = () => {
 		// MAKE INTERACTIVE
-		// document.removeEventListener("dblclick", this.onDocumentDoubleClick);
+		document.removeEventListener("dblclick", this.onDocumentDoubleClick);
 		window.removeEventListener('resize', this.handleWindowResize);
 	};
 
@@ -516,11 +516,11 @@ class CubeEnvironment extends Component {
 
 				// MAKE INTERACTIVE
 				// Set the overlay and project
-				// this.setState({
-				// 	pause: true,
-				// 	showOverlay: true,
-				// 	overlayProject: mesh.object.userData.project
-				// });
+				this.setState({
+					pause: true,
+					showOverlay: true,
+					overlayProject: mesh.object.userData.project
+				});
 			}
 		}
 	};
